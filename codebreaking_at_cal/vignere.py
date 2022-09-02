b1925ed2ed459f3dc23e64a882b620c0de8e3ab0
Ryan Cottone <rcottone@berkeley.edu>
2022-09-01 21:00:28 -0700

    vignere test

--- codebreaking_at_cal/vignere.py
+++ codebreaking_at_cal/vignere.py
@@ -2,8 +2,17 @@ from .caesar import *
 from .utils import *
 import itertools
 from collections import defaultdict
+import csv
+import random
 
 MAX_KEYLENGTH = 20
+
+words = []
+
+with open("./data/common_words.csv", "r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    for word in csvreader:
+        words.append(word)
 
 base_keylen_proportions = [0.26858405, 0.15936355, 0.10731316, 0.07769914, 0.06053305,
        0.05064498, 0.03970286, 0.0334287 , 0.02881092, 0.02584952,
@@ -31,3 +40,43 @@ def vignere_decrypt(ciphertext, key):
     for i in range(len(ciphertext)):
         decrypted += shift_letter(ciphertext[i], -char_to_num(padded_key[i]))
     return decrypted
+
+def test_vignere_cracker(func):
+    
+    for i in range(50):
+        chosenWords = random.sample(words, random.randint(100, 900))
+
+        text = ' '.join(chosenWords)
+
+        ciphertext = vignere_encrypt(text)
+
+        decrypted = func(ciphertext)
+
+        if decrypted != text:
+            print('incorrect!')
+
+import random, csv
+
+def test_vignere_cracker(func, iterations):
+    correct = 0
+
+    for i in range(iterations):
+        chosenWords = random.sample(words, random.randint(100, 900))
+        chosenKey = random.sample(alphabet, random.randint(2,15))
+        text = ""
+        key = ""
+
+        for word in chosenWords:
+            text+= word[0] + ' '
+        
+        for letter in chosenKey:
+            key+=letter[0]
+
+        ciphertext = vignere_encrypt(text, key)
+
+        decrypted = func(ciphertext)
+        
+        if decrypted[0:10] == text[0:10]:
+            correct+=1
+    
+    return correct/iterations
